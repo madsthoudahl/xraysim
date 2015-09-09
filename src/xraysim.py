2d398c3e405b1176f188091e64a750411bf40f8e
Mads Thoudahl <thoudahl@gmail.com>
2015-09-09 21:10:02 +0200

    geometric calculations moved to geometry file

--- src/xraysim.py
+++ src/xraysim.py
@@ -9,26 +9,30 @@ Created on Mon Aug 31 14:14:15 2015
 """
 import numpy as np
 from xraysimphysics import randmaterialAAscene, materials
-from xraysimgeometry import coordsAAscene, raygeometry, detectorgeometry
+from xraysimgeometry import coordsAAscene, raygeometry, \
+                            detectorgeometry, runAABB, runAABBcompact
 
 ## constants
 eps = 1e-6 #fault tolerance epsilon
+invsqr = 1.0 / 4 * np.pi
 
 ## indexing constants
 power = 3
 energylevel = 4
 pixelpositions = 0
 normvec = 1
+pixelarea = 2
+result = 3
 
 ## simulation variables
-scene_size = ss = 60 #is cubed!
-res1       = 20 # y-dir?
-res2       = 20 # z-dir?
+scene_size = ss = 10 #is cubed!
+res1       = 10 # y-dir?
+res2       = 10 # z-dir?
 #(60**3)*20*20 = memory error :-) @ 8GB
 
 asource = np.array([
               0.0, 0.0, 0.0, # position
-              10,    # Power [Joules]
+              1,     # Power [relative to other sources]
               80     # Energy level of xrays [keV]
              ])
 
@@ -62,6 +66,7 @@ def xraysim_benchmark(
     # generate a random scene from scene definitions
     scenematerials = randmaterialAAscene(scenedefs)
     scenegrid = coordsAAscene(scenedefs)
+    sgs = np.array(scenegrid.shape)[1:4] -1 #just xyz dims and voxel# not edge#
     print "axisaligned scene generated"
 
     # generate an array of endpoints for rays (at the detector)
@@ -71,6 +76,7 @@ def xraysim_benchmark(
 
 
     for source in sourcelist:
+        _sox, _soy, _soz, spower, senergy = source
         # preprocess the scene physics
 
         # building a map of attenuation coefficients
@@ -78,75 +84,48 @@ def xraysim_benchmark(
 
         for material_id in np.arange(len(materials)):
             materialattenuationfun = materials[material_id][1]
-            sceneattenuates += (scenematerials == material_id) * materialattenuationfun(source[energylevel])
+            sceneattenuates += (scenematerials == material_id) * materialattenuationfun(senergy)
 
         print "attenuation map for source at {0} generated".format(source[0:3])
 
         # prepare scene geometry
         rayorigin  = source[0:3]
 
-        for detector in detectors:
+        for pixelpositions, pixelareavector, dshape, result in detectors:
             # do geometry
-            rayudirs, raydists = raygeometry(rayorigin, detector[pixelpositions])
-            ray_inverse = 1.0 / rayudirs
-            print "raydirections to detector source at {0} generated".format(detector[pixelpositions][0,0])
+            rayudirs, raylengths, rayinverse = raygeometry(rayorigin, pixelpositions)
+            print "raydirections to detector source at {0} generated".format( pixelpositions[0,0] )
 
-#            ## AABB algorithm... (AxisAlignedBoundingBox)
-#            # call function implemented in xraysimgeometry??
-#            ts = np.empty((6,ss,ss,ss,ray_inverse.shape[0]))
-#            txs, tys, tzs = ts[0:2], ts[2:4], ts[4:6]
-#
-#            txs[0] = ( scenegrid[0][:-1,:-1,:-1].reshape(ss,ss,ss,1) - rayorigin[0] ) * ray_inverse[:,0]
-#            txs[1] = ( scenegrid[0][1:,1:,1:].reshape(ss,ss,ss,1) - rayorigin[0] ) * ray_inverse[:,0]
-#
-#            tys[0] = ( scenegrid[1][:-1,:-1,:-1].reshape(ss,ss,ss,1) - rayorigin[1] ) * ray_inverse[:,1]
-#            tys[1] = ( scenegrid[1][1:,1:,1:].reshape(ss,ss,ss,1) - rayorigin[1] ) * ray_inverse[:,1]
-#
-#            tzs[0] = ( scenegrid[2][:-1,:-1,:-1].reshape(ss,ss,ss,1) - rayorigin[2] ) * ray_inverse[:,2]
-#            tzs[1] = ( scenegrid[2][1:,1:,1:].reshape(ss,ss,ss,1) - rayorigin[2] ) * ray_inverse[:,2]
-#
-#
-#            t_in = np.max(np.array([np.min(txs, axis=0),np.min(tys, axis=0),np.min(tzs, axis=0)]),axis=0)
-#            t_out = np.min(np.array([np.max(txs, axis=0),np.max(tys, axis=0),np.max(tzs, axis=0)]),axis=0)
-#
-#            raydst1 = (t_out-t_in)*(t_out>=t_in)
-#            #raydst.shape should be (ss, ss, ss, res1*res2) :-)
-
-
-            #COMPACT AABB ALGORITHM
-            sgs = scenegrid.shape
-            if sgs[1]==sgs[2]==sgs[3]: ##compact is safe
-
-                ## Initialize & name aliases for calculation array
-                tss = np.empty((6,ss,ss,ss,ray_inverse.shape[0]))
-                txs, tys, tzs = tss[0:2], tss[2:4], tss[4:6]
-
-                ## Ray Geometry
-                tss[::2] = ( scenegrid[:,:-1,:-1,:-1] - rayorigin.reshape(3,1,1,1) ).reshape(3,ss,ss,ss,1) * ray_inverse[:,:].T.reshape(3,1,1,1,res1*res2)
-                tss[1::2] = ( scenegrid[:,1:,1:,1:] - rayorigin.reshape(3,1,1,1) ).reshape(3,ss,ss,ss,1) * ray_inverse[:,:].T.reshape(3,1,1,1,res1*res2)
-
-                ## Figure incoming and outgoing t_values (t is travelled distance) at each voxel
-                t_in = np.max([np.min(txs, axis=0),np.min(tys, axis=0),np.min(tzs, axis=0)],axis=0)
-                t_out = np.min([np.max(txs, axis=0),np.max(tys, axis=0),np.max(tzs, axis=0)],axis=0)
-
-                ## Figure distances travelled in each voxel
-                raydst = (t_out-t_in)*(t_out>=t_in) ## shape (ss,ss,ss,res1*res2)
-
-#                ## Determine if compact calculation is the same as
-#                print "Compact is valid: {}".format(np.max(raydst1-raydst)<eps)
-
-
+            if sgs[0]==sgs[1]==sgs[2]: ##compact is safe
+                #?COMPACT? AABB ALGORITHM
+                raydst = runAABBcompact(scenegrid, rayudirs, rayorigin, rayinverse)
+            else:
+                #Normal AABB ALGORITHM
+                raydst = runAABB(scenegrid, rayudirs, rayorigin, rayinverse)
 
             #raydst is now to be correlated with material/attenuation grid
+#            print "shapes:"
+#            print "pixelareavector:\t{}".format(pixelareavector)
+
+            dtectattenuates = np.sum((sceneattenuates.reshape(sgs[0],sgs[1],sgs[2],1)* raydst).reshape(sgs[0]*sgs[1]*sgs[2],dshape[0],dshape[1]), axis = 0) #shape?
+#            print "dtectattenuates:\t{}".format(dtectattenuates.shape)
+
+            pixelintensity = (source[power] * invsqr / raylengths).reshape(dshape) #shape?
+#            print "pixelintensity:\t{}".format(pixelintensity.shape)
+
+            area = np.dot( rayudirs, pixelareavector.reshape(3,1) ).reshape(dshape) #correct? #shapes
+#            print "area:\t\t\t{}".format(area.shape)
+
+            result += pixelintensity * area * np.exp(- dtectattenuates) #shapes
 
 
     print "end of xraysim"
 
-    return rayudirs, raydists , detectors, sceneattenuates, scenegrid, ray_inverse, rayorigin, ts, t_out, t_in, raydst
+    return rayudirs, raydists , detectors, sceneattenuates, scenegrid, ray_inverse, rayorigin, tss, t_out, t_in, raydst
 
 
 if __name__ == '__main__':
-    rayudirs, raydists, detectors, sceneattenuates, scenegrid, ray_inverse, rayorigin, ts, t_out, t_in, raydst = xraysim_benchmark()
-    txs, tys, tzs = ts[0:2], ts[2:4], ts[4:6]
+    rayudirs, raydists, detectors, sceneattenuates, scenegrid, ray_inverse, rayorigin, tss, t_out, t_in, raydst = xraysim_benchmark()
+    txs, tys, tzs = tss[0:2], tss[2:4], tss[4:6]
 
 
--- src/xraysimgeometry.py
+++ src/xraysimgeometry.py
@@ -7,9 +7,9 @@ Created on Mon Aug 31 14:38:12 2015
 """
 import numpy as np
 
-def normalizevector(vector):
-    """ returns a vector with frombenius norm = 1 """
-    return vector * (1.0 / np.linalg.norm(vector))
+#def normalizevector(vector):
+#    """ returns a vector with frombenius norm = 1 """
+#    return vector * (1.0 / np.linalg.norm(vector))
 
 
 def coordsAAscene(scenedefs, samexyzres=True):
@@ -19,20 +19,20 @@ def coordsAAscene(scenedefs, samexyzres=True):
     xres, yres, zres = scenedefs[6:9] # resolution
 
     samexyzres = (xres == yres == zres)
-        
+
     if not samexyzres:
         xgrid = np.linspace(x0, x1, xres+1)
         ygrid = np.linspace(y0, y1, yres+1)
         zgrid = np.linspace(z0, z1, zres+1)
         return np.meshgrid(xgrid,ygrid,zgrid)
 
-    res = xres + 1    
+    res = xres + 1
     grid = np.empty((3,res,res,res))
     grid[0] = np.repeat(np.repeat(np.linspace(x0, x1, res).reshape(res,1),res,axis=1).reshape(res,res,1),res,axis=2)
     grid[1] = np.repeat(np.repeat(np.linspace(y0, y1, res).reshape(res,1),res,axis=1).reshape(res,res,1),res,axis=2)
     grid[2] = np.repeat(np.repeat(np.linspace(z0, z1, res).reshape(res,1),res,axis=1).reshape(res,res,1),res,axis=2)
-    return grid    
-    
+    return grid
+
 
 def raygeometry(src, detpixpos):
     """ calculates an array of normalized (length=1) vectors representing
@@ -40,19 +40,22 @@ def raygeometry(src, detpixpos):
         pixel positions
 
         parameters:
-            src        is source position
-            detpixpos  is a 3D array, 2 pixel dimensions,
-                       one spatial xyz positions
+            src          is source position
+            detpixpos    is a 3D array, 2 pixel dimensions,
+                         one spatial xyz positions
         returns:
-            unitrays   is the ray directions in unit lengths,
-                       flattened to 1 pixel dimension, and 1 spatial dimension
-            distances  is the distances belonging to the ray-vector
+            unitrays     is the ray directions in unit lengths,
+                         flattened to 1 pixel dimension, and 1 spatial dimension
+            distances    is the distances belonging to the ray-vector
+            rayinverses  serves to minimize division operations
         """
-    rayshp = detpixpos.shape
-    raycount = rayshp[0]*rayshp[1]
-    rays = detpixpos.reshape(raycount,3)[:,0:3] - src[0:3]
-    dists = np.sqrt(np.sum(rays*rays,1)).reshape(raycount,1)
-    return rays * 1.0/dists, dists
+    rayshp         = detpixpos.shape
+    raycount       = rayshp[0]*rayshp[1]
+    rays           = detpixpos.reshape(raycount,3)[:,0:3] - src[0:3]
+    raydistances   = np.sqrt(np.sum(rays*rays,1)).reshape(raycount,1)
+    unitrays       = rays*1.0/raydistances
+    rayinverses    = 1.0 / unitrays
+    return  unitrays, raydistances, rayinverses
 
 
 def detectorgeometry(ddef, npdtype='float32'):
@@ -83,7 +86,55 @@ def detectorgeometry(ddef, npdtype='float32'):
     pzs  = np.fromfunction(pcfunz, shape=dshape, dtype=npdtype )
     pixelpositions = np.dstack((pxs,pys,pzs)) # shape = (r2,r1,3)
 
-    normalvector = np.cross(di,dj)
-    unitnormalvector = normalizevector(normalvector)
+#    normalvector = np.cross(di,dj)
+#    unitnormalvector = normalizevector(normalvector)
+    pixelareavector = np.cross(di, dj)
+    result = np.zeros(dshape)
 
-    return (pixelpositions, unitnormalvector)
+    return  pixelpositions, pixelareavector, dshape, result
+
+
+def runAABB(scenegrid, rayudirs, rayorigin, rayinverse):
+    """ Returns the distances that every ray travels in every voxel """
+    ssx, ssy, ssz = np.array(scenegrid.shape[1:4]) - 1
+    ts = np.empty((6, ssx, ssy, ssz, rayinverse.shape[0]))
+    txs, tys, tzs = ts[0:2], ts[2:4], ts[4:6]
+
+
+    txs[0] = ( scenegrid[0][:-1,:-1,:-1].reshape(ssx, ssy, ssz, 1) - rayorigin[0] ) * rayinverse[:,0]
+    txs[1] = ( scenegrid[0][1:,1:,1:].reshape(ssx, ssy, ssz, 1) - rayorigin[0] ) * rayinverse[:,0]
+
+    tys[0] = ( scenegrid[1][:-1,:-1,:-1].reshape(ssx, ssy, ssz, 1) - rayorigin[1] ) * rayinverse[:,1]
+    tys[1] = ( scenegrid[1][1:,1:,1:].reshape(ssx, ssy, ssz, 1) - rayorigin[1] ) * rayinverse[:,1]
+
+    tzs[0] = ( scenegrid[2][:-1,:-1,:-1].reshape(ssx, ssy, ssz, 1) - rayorigin[2] ) * rayinverse[:,2]
+    tzs[1] = ( scenegrid[2][1:,1:,1:].reshape(ssx, ssy, ssz, 1) - rayorigin[2] ) * rayinverse[:,2]
+
+
+    t_in = np.max(np.array([np.min(txs, axis=0),np.min(tys, axis=0),np.min(tzs, axis=0)]),axis=0)
+    t_out = np.min(np.array([np.max(txs, axis=0),np.max(tys, axis=0),np.max(tzs, axis=0)]),axis=0)
+
+    voxelraydistances = (t_out-t_in)*(t_out>=t_in)
+    return voxelraydistances
+
+
+def runAABBcompact(scenegrid, rayudirs, rayorigin, rayinverse):
+    """ Returns the distances that every ray travels in every voxel"""
+    ss, _ssy, _ssz = np.array(scenegrid.shape[1:4]) - 1
+
+    raycount = rayinverse.shape[0]
+    ## Initialize & name aliases for calculation array
+    tss = np.empty((6, ss, ss, ss, raycount))
+    txs, tys, tzs = tss[0:2], tss[2:4], tss[4:6]
+#    print "aabbcompactsg = {}, rayorigin: {}".format(scenegrid.shape, rayorigin.shape)
+    ## Ray Geometry
+    tss[::2] = ( scenegrid[:,:-1,:-1,:-1] - rayorigin.reshape(3,1,1,1) ).reshape(3,ss,ss,ss,1) * rayinverse.T.reshape(3,1,1,1,raycount)
+    tss[1::2] = ( scenegrid[:,1:,1:,1:] - rayorigin.reshape(3,1,1,1) ).reshape(3,ss,ss,ss,1) * rayinverse[:,:].T.reshape(3,1,1,1,raycount)
+
+    ## Figure incoming and outgoing t_values (t is travelled distance) at each voxel
+    t_in = np.max([np.min(txs, axis=0),np.min(tys, axis=0),np.min(tzs, axis=0)],axis=0)
+    t_out = np.min([np.max(txs, axis=0),np.max(tys, axis=0),np.max(tzs, axis=0)],axis=0)
+
+    ## Figure distances travelled in each voxel
+    voxelraydistances = (t_out-t_in)*(t_out>=t_in)
+    return voxelraydistances
